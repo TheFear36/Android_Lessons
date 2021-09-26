e4f16cdaa5baa25a6323e0e2980991f295b1cb19
TheFear36 <gpodvorotov@gmail.com>
2021-09-26 22:41:26 +0300

    First home work with using the initiative

---
+++ app/src/main/java/com/thefear/homework/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.thefear.homework;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.thefear.homework.databinding.ActivitySecond2Binding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+    }
+}
